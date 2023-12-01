ddc94abf8d6df01185102db2b7214f4971f58040
Jatin <jatin95017@gmail.com>
2023-12-01 16:52:23 +0000

    New changes implemented

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -106,11 +106,16 @@
                                     </li>
                                     <li role="separator" class="divider"></li>
 										<i class="mdi mdi-power"></i> Logout
-                                    <li><a href="{{ route('edit-my-profile.edit', Auth::user()->id) }}"><i class="mdi mdi-account"></i> My Profile</a></li>
+                                    <li>
+                                    	<a href="{{ route('edit-my-profile.edit', Auth::user()->id) }}"><i class="mdi mdi-account"></i> My Profile</a>
+                                    </li>
                                     <!-- <li><a href="#"><i class="ti-settings"></i> Account Setting</a></li> -->
                                     <li>
-									@if(Auth::user()->role_id == 1)
-										<a href="{{ route('edit-my-profile.edit', Auth::user()->id) }}" >Profile</a>
+										@if(Auth::user()->role_id == 1)
+											<a href="{{ route('edit-my-profile.edit', Auth::user()->id) }}" >Profile</a>
+										@endif
+										@if(Auth::user()->role_id == 2)
+											<a href="{{ route('my-profile.edit', Auth::user()->id) }}" >Profile</a>
 										@endif
 										<a href="#"
 										onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
