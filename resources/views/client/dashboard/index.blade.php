18a5f05a0cb917599e993b048c6b20a81e1e5b14
Jatin <jatin95017@gmail.com>
2025-01-15 22:35:18 +0530

    Dashboard changes

--- resources/views/client/dashboard/index.blade.php
+++ resources/views/client/dashboard/index.blade.php
@@ -97,7 +97,7 @@
                                 ?>
                                 <div class="custom-calender">
                                     <input type="text" class="form-control" name="daterange"
-                                        value="01/01/2018 - 01/15/2018" />
+                                        value="01/01/2024 - 01/15/2024" />
                                 </div>
                             </div>
                         </div>
